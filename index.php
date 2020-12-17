b8185082d250adc8fb888566d091b23db7f691e4
Alan Silva <alan.silva@jaime.com.br>
2020-12-17 13:16:03 -0300

    Codigo index.html identado

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
     <title>Projeto Upload de foto com ajax</title>
 </head>
 <body>
-Nome:<br/>
-<input type="text" id="nome" name="nome" /><br/><br/>
+	Nome:<br/>
+	<input type="text" id="nome" name="nome" /><br/><br/>
 
-Foto:<br/>
-<input type="file" id="foto" name="foto" /><br/><br/>
+	Foto:<br/>
+	<input type="file" id="foto" name="foto" /><br/><br/>
 
-<button>Enviar</button>
+	<button>Enviar</button>
 
-<script type="text/javascript" src="jquery.min.js"></script>
-<script type="text/javascript" src="script.js"></script>
+	<script type="text/javascript" src="jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
