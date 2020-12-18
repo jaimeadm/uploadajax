f2a1a2159ed30a5e02f93829ba3793fdfc313eee
Alan Silva <alan.silva@jaime.com.br>
2020-12-18 16:01:49 -0300

    Nova funcao!

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<html>
-<head>
-    <title>Projeto Upload de foto com ajax - 1.0</title>
-</head>
-<body>
-	<h1>Upload de imagem - 1.0 <small>ajax e php</small></h1>
-	Nome:<br/>
-	<input type="text" id="nome" name="nome" /><br/><br/>
-
-	Foto:<br/>
-	<input type="file" id="foto" name="foto" /><br/><br/>
-
-	<button>Enviar</button>
-
-	<script type="text/javascript" src="jquery.min.js"></script>
-	<script type="text/javascript" src="script.js"></script>
-</body>
-</html>
+erro
