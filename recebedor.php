a52169c822704f463a12c0783b45031e6a8601a9
Alan Silva <alan.silva@jaime.com.br>
2020-12-18 15:19:31 -0300

    Alterado o nome da pasta fotos para imagens

--- recebedor.php
+++ recebedor.php
@@ -2,7 +2,7 @@
 if(isset($_FILES['foto'])) {
 
 	$arquivo = $_FILES['foto'];
-	move_uploaded_file($arquivo['tmp_name'], 'fotos/'.$arquivo['name']);
+	move_uploaded_file($arquivo['tmp_name'], 'imagens/'.$arquivo['name']);
 
 	echo "Arquivo ".$_POST['nome']." enviado com sucesso!";
 
